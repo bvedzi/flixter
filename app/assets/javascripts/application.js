5df3bf9ff1ab25778d70ee1ccb2042168f164226
Brian Vedzi <vdzvsk@gmail.com>
2019-11-04 16:59:04 +0000

    js lesson 31

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,39 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+//= require jquery-ui/core
+//= require jquery-ui/widget
+//= require jquery-ui/position
+
+//= require jquery-ui/widgets/datepicker
+//= require jquery-ui/widgets/datepicker
+//= require jquery-ui/i18n/datepicker-pt-BR
+
+//= require jquery-ui/widgets/mouse
+//= require jquery-ui/widgets/draggable
+//= require jquery-ui/widgets/droppable
+//= require jquery-ui/widgets/resizable
+//= require jquery-ui/widgets/selectable
+//= require jquery-ui/widgets/sortable
+
+//= require jquery-ui/widgets/accordion
+//= require jquery-ui/widgets/autocomplete
+//= require jquery-ui/widgets/button
+//= require jquery-ui/widgets/datepicker
+//= require jquery-ui/widgets/dialog
+//= require jquery-ui/widgets/menu
+//= require jquery-ui/widgets/progressbar
+//= require jquery-ui/widgets/selectmenu
+//= require jquery-ui/widgets/slider
+//= require jquery-ui/widgets/spinner
+//= require jquery-ui/widgets/tabs
+//= require jquery-ui/widgets/tooltip
+
+//= require jquery-ui
+//= require jquery-ui/effect.all
+
 //= require rails-ujs
 //= require activestorage
 //= require jquery
