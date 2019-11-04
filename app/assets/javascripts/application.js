2c3fb41526c8569462ae38119541e2643e36ba88
Brian Vedzi <vdzvsk@gmail.com>
2019-11-04 22:47:20 +0000

    starting lesson 33

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,19 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require rails-ujs
-//= require activestorage
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-
-//= require jquery3
 //= require popper
+//= require tether
 //= require bootstrap-sprockets
-
+//= require_tree .
 //= require jquery-ui
-//= require jquery-ui/effect.all
-
-
