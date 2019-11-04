db444cb6e1ca2627b0a33d72cd804a63799d1c20
Brian Vedzi <vdzvsk@gmail.com>
2019-11-04 20:12:36 +0000

    Put/Sorting not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,38 +11,6 @@
 // about supported directives.
 //
 
-
-//= require jquery-ui/core
-//= require jquery-ui/widget
-//= require jquery-ui/position
-
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/i18n/datepicker-pt-BR
-
-//= require jquery-ui/widgets/mouse
-//= require jquery-ui/widgets/draggable
-//= require jquery-ui/widgets/droppable
-//= require jquery-ui/widgets/resizable
-//= require jquery-ui/widgets/selectable
-//= require jquery-ui/widgets/sortable
-
-//= require jquery-ui/widgets/accordion
-//= require jquery-ui/widgets/autocomplete
-//= require jquery-ui/widgets/button
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/dialog
-//= require jquery-ui/widgets/menu
-//= require jquery-ui/widgets/progressbar
-//= require jquery-ui/widgets/selectmenu
-//= require jquery-ui/widgets/slider
-//= require jquery-ui/widgets/spinner
-//= require jquery-ui/widgets/tabs
-//= require jquery-ui/widgets/tooltip
-
-//= require jquery-ui
-//= require jquery-ui/effect.all
-
 //= require rails-ujs
 //= require activestorage
 //= require jquery
@@ -53,3 +21,8 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+//= require jquery-ui
+//= require jquery-ui/effect.all
+
+
